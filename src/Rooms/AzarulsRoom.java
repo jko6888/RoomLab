53a4e76dca0e84855219c5664f4f3c4a390f04c7
BT_1N3_21 <jko6888@bths.edu>
2018-11-01 11:36:51 -0400

    Kevin1

--- src/Rooms/AzarulsRoom.java
+++ src/Rooms/AzarulsRoom.java
@@ -1,4 +1,28 @@
 package Rooms;
 
-public class AzarulsRoom {
+import Game.Runner;
+import People.Person;
+
+public class AzarulsRoom extends Room
+{
+
+    public AzarulsRoom(int x, int y) {
+        super(x, y);
+
+    }
+
+    /**
+     * Triggers the game ending conditions.
+     * @param x the Person entering
+     */
+    @Override
+    public void enterRoom(Person x) {
+
+        occupant = x;
+        x.setxLoc(this.xLoc);
+        x.setyLoc(this.yLoc);
+        System.out.println("You caught is enraged as he loses in Nitrotype! He punches you as you enter. Lose 10 Health.");
+    }
+
+
 }
