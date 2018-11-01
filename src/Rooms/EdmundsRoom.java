8a0501787f4c414872222f7a8e4f960dc90555be
BT_1N3_21 <jko6888@bths.edu>
2018-11-01 11:46:36 -0400

    Kevin2

--- src/Rooms/EdmundsRoom.java
+++ src/Rooms/EdmundsRoom.java
@@ -1,4 +1,28 @@
 package Rooms;
 
-public class EdmundsRoom {
+import Game.Runner;
+import People.Person;
+
+public class EdmundsRoom extends Room
+{
+
+    public EdmundsRoom(int x, int y) {
+        super(x, y);
+
+    }
+
+    /**
+     * Triggers the game ending conditions.
+     * @param x the Person entering
+     */
+    @Override
+    public void enterRoom(Person x) {
+
+        occupant = x;
+        x.setxLoc(this.xLoc);
+        x.setyLoc(this.yLoc);
+        System.out.println("Kevin farted in the room! You lose 20 health");
+    }
+
+
 }
